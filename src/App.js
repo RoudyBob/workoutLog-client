45750808bd02c73043872edef97683011bc6079f
Bob Roudebush <bob@roudybob.net>
2021-02-06 11:02:11 -0500

    removed unused import

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import Sitebar from './home/Navbar';
 import Auth from './auth/Auth';
-import WorkoutIndex from './workouts/WorkoutIndex';
-import { Button } from 'reactstrap';
+import WorkoutIndex from './workouts/WorkoutIndex'
 
 function App() {
 
